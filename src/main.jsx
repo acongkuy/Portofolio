74cc6908883820e6feeb42850a394c2fc0158d28
chunsky <walnutacong@gmail.com>
2026-04-23 00:05:41 +0700

    wjwjwjw

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './app.jsx'
 import './index.css'
 
-// Fungsi ini menghubungkan kode React ke elemen <div id="root"> yang ada di index.html
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
